624c3f29fbfb9de2a0367c7224edb7e878b4f593
Nicolás Manso Nuñez <miguel.manso@uah.es>
2026-01-18 17:06:13 +0100

    Fin de la parte de imágenes

---
+++ imagenes/avanzadas/ejercicios/phpinfo/phpinfo/index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Muestra todas las informaciones, como lo haría INFO_ALL
+phpinfo();
+
+// Muestra únicamente el módulo de información.
+// phpinfo(8) proporcionaría las mismas informaciones.
+phpinfo(INFO_MODULES);
+
+?>
